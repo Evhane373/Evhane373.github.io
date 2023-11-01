c2e4c09c603ec1ef573988b64817a753d4b7365b
Evhane-Mxix <90011399+Evhane373@users.noreply.github.com>
2023-10-31 23:53:49 -0400

    Update Day2.js

--- AoC/2015/Day2.js
+++ AoC/2015/Day2.js
@@ -19,12 +19,15 @@ function Day2Part1(input)
 function Day2Part2()
 {
    let inputArray = input.split("\n");
+   let ribbon = 0;
    for(let x = 0; x < inputArray.length; x++)
    {
       let sides = inputArray[x].split("x");
       sides.sort(function(a, b){return b - a});
       
-      
+      let wrap = 2 * sides[0] + 2 * sides[1];
+      let bow = sides[0] + sides[1] + sides[2];
+      ribbon += wrap + bow;
    }
-   
+   return ribbon;
 }
