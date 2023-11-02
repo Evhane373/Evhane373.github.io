ff820ed77cccfce8568010b294df03342458a89f
OSEvanJ <ewjocque@yahoo.com>
2023-11-02 11:27:02 -0400

    Update Dar3.js

--- AoC/2015/Dar3.js
+++ AoC/2015/Dar3.js
@@ -1,8 +1,44 @@
-function Day3Part1()
+function Day3Part1(input)
 {
-
+	let location = [0, 0];
+	let visited = [];
+	let houses = 1;
+	for(let x = 0; x < input.length; x++)
+	{
+		if(input.charAt(x) == "^")
+		{
+			location[0]++;
+		}
+		else if(input.charAt(x) == "v")
+		{
+			location[0]--;
+		}
+		else if(input.charAt(x) == "<")
+		{
+			location[0]--;
+		}
+		else if(input.charAt(x) == ">")
+		{
+			location[0]++;
+		}
+		
+		let newHouse = true;
+		for(let y = 0; y < visited.length; y++)
+		{
+			if(location == visited[y])
+			{
+				newhouse = false;
+			}
+		}
+		if(newhouse == true)
+		{
+			houses++;
+		}
+		visited.push(location);
+	}
+	return houses;
 }
-function Day3Part2()
+function Day3Part2(input)
 {
 	
 }
