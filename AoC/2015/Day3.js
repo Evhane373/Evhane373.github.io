10cbed9d96f2ba31622829342e039d7ea8a4aa59
OSEvanJ <ewjocque@yahoo.com>
2023-11-03 11:03:09 -0400

    Update Day3.js

--- AoC/2015/Day3.js
+++ AoC/2015/Day3.js
@@ -29,6 +29,7 @@ function Day3Part1(input)
 			if(location.toString() == visited[y].toString())
 			{
 				newHouse = false;
+				console.log(location.toString() + " " +  visited[y].toString()
 			}
 			console.log(newHouse);
 		}
