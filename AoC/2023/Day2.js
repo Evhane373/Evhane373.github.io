3d4e718201b5447e36775bd00a308c6a752ed47b
Evhane-Mxix <90011399+Evhane373@users.noreply.github.com>
2023-12-02 13:42:20 -0500

    Update Day2.js

--- AoC/2023/Day2.js
+++ AoC/2023/Day2.js
@@ -1,11 +1,31 @@
 function Day2Part1(input) 
 {
-	game = input.split(":")[0];
-   console.log(game);
-   rolls = input.split(":")[1].split(";");
-   console.log(rolls);
+   let inputLine = input.split('\n');
+   let total = 0;
    
-   return(game + rolls[0]);
+   for(let x = 0; x < inputLine.length; x++)
+   {
+      game = inputLine[x].split(':')[0];
+      let valid = true;
+      rolls = game[1].split(';');
+      for(let y = 0; y < 3; y++)
+      {
+         round = rolls[y].split(' ');
+         for(let z = 0; z < round.length; z+=2)
+         {
+            if((round[z] > 12 && round[z] == 'red') || (round[z] > 13 && round[z] == 'green') || (round[z] > 14 && round[z] == 'blue'))
+            {
+               valid = false;
+            }
+         }
+      }
+      if(valid)
+      {
+         total += game[1];
+      }
+   }
+   
+   return(total);
 }
 function Day2Part2(input)
 {
