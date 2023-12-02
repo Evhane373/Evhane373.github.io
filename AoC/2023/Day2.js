800b74d4d69c0cf1a3cadce441401413956723b2
Evhane-Mxix <90011399+Evhane373@users.noreply.github.com>
2023-12-02 09:03:54 -0500

    Update Day2.js

--- AoC/2023/Day2.js
+++ AoC/2023/Day2.js
@@ -1,6 +1,11 @@
 function Day1Part1(input) 
 {
-	
+	game = input.split(":")[0];
+   console.log(game);
+   rolls = input.split(":")[1].split(";");
+   console.log(rolls);
+   
+   return(game);
 }
 function Day1Part2(input)
 {
