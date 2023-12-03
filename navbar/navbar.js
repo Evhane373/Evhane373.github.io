1224f577d1452955d809c2c3b7bd10120f254c27
Evhane-Mxix <90011399+Evhane373@users.noreply.github.com>
2023-12-02 22:37:32 -0500

    Update navbar.js

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -1,6 +1,19 @@
 function insertNav(currentPage)
 {
-   document.getElementByID("navbar").innerHTML = loadNavbar();
+   document.getElementByID("navbar").innerHTML = 
+   `<link rel="stylesheet" href="navbar.css">
+      <div class="navbar">
+         <a href="/">Home</a>
+         <div class="dropdown">
+            <button class="dropbtn">Advent of Code
+               <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+               <a href="/AoC/2015/AoC2015.html">2015</a>
+               <a href="/AoC/2023/AoC2023.html">2023</a>
+            </div>
+         </div>
+      </div>`;
 }
 
 async function loadNavbar() 
