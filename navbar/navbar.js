7e8a8d1878557964988af7874fbfd2bac79bfcb7
Evhane-Mxix <90011399+Evhane373@users.noreply.github.com>
2023-12-02 23:05:05 -0500

    Update navbar.js

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -1,5 +1,5 @@
 function insertNav(currentPage)
 {
    const target = document.getElementById("navbar");
-   target.innerHTML = '<object width="100%" type="text/html" data="/navbar/navbar.html"</object>';
+   target.innerHTML = '<object width="100%" type="text/html" data="/navbar/navbar.html"></object>';
 }
