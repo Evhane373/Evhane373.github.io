cb91ac9dd52ace6c0193d9c0d07ed0a95f3c1fef
Evhane-Mxix <90011399+Evhane373@users.noreply.github.com>
2023-12-02 20:06:54 -0500

    Update Day2.js

--- AoC/2023/Day2.js
+++ AoC/2023/Day2.js
@@ -66,15 +66,15 @@ function Day2Part2(input)
          for(let z = 0; z < round.length; z+=2)
          {
             console.log(round[z] + "|" + round[z+1]);
-            if(round[z] > redMin && round[z+1] == 'red')
+            if(round[z] > redMin && round[z+1].trim() == 'red')
             {
                redMin = round[z];
             }
-            if(round[z] > greenMin && round[z+1] == 'green')
+            if(round[z] > greenMin && round[z+1].trim() == 'green')
             {
                greenMin = round[z];
             }
-            if(round[z] > blueMin && round[z+1] == 'blue')
+            if(round[z] > blueMin && round[z+1].trim() == 'blue')
             {
                blueMin = round[z];
             }
